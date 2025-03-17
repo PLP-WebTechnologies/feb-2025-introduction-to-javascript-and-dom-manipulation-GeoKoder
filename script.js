cca2a4c463773abccf31e0e84c798d2ea7f15a31
Geo Koder <143654081+GeoKoder@users.noreply.github.com>
2025-03-17 08:23:23 +0300

    Updated some necessary functionality

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const changeTextBtn = document.getElementById("changeTextBtn");
 const toggleElementBtn = document.getElementById("toggleElementBtn");
 const titleStyleBtn = document.getElementById("titleStyleBtn");
+const greet = document.getElementById("greet");
+
+//Interacted with the user
+let name = prompt("What is your name, dear web user?");
+const greeting = document.createElement("p");
+greeting.textContent = `Hello ${name}, happy to meet you!`;
+greet.appendChild(greeting);
 
 changeTextBtn.addEventListener("click", () => {
   // updates p text content then changes button style
@@ -10,6 +17,7 @@ changeTextBtn.addEventListener("click", () => {
 });
 
 toggleElementBtn.addEventListener("click", () => {
+  //add or remove element functionality
   const container = document.getElementById("container");
   let existingElement = document.getElementById("newElement");
   if (existingElement) {
@@ -23,6 +31,7 @@ toggleElementBtn.addEventListener("click", () => {
 });
 
 titleStyleBtn.addEventListener("click", () => {
+  //change text style functionality
   const title = document.getElementById("title");
   title.style.color = "red";
   title.style.fontFamily = "cursive";
